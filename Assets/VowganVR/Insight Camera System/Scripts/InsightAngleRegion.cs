d1c3cc7ab704ff17073ede28655a947454dd4b12
dongho shin <dongho5309@belivvr.com>
2022-10-10 23:03:04 +0900

    VowganVR Third Cam on PC

---
+++ Assets/VowganVR/Insight Camera System/Scripts/InsightAngleRegion.cs
@@ -0,0 +1,36 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+namespace VowganVR
+{
+    [UdonBehaviourSyncMode(BehaviourSyncMode.Manual)]
+    public class InsightAngleRegion : UdonSharpBehaviour
+    {
+        
+        public bool BlendCamera;
+        public InsightController Controller;
+        public Transform Target;
+        public Transform LookAt;
+        
+        private bool directing;
+        
+        
+        public override void OnPlayerTriggerEnter(VRCPlayerApi player)
+        {
+            if (!player.isLocal) return;
+            if (directing) return;
+            directing = true;
+            Controller.StartDirection(Target, LookAt, BlendCamera, false);
+        }
+        
+        public override void OnPlayerTriggerExit(VRCPlayerApi player)
+        {
+            if (!player.isLocal) return;
+            directing = false;
+            Controller.EndDirection(false);
+        }
+    }
+}
---
+++ Assets/VowganVR/Insight Camera System/Scripts/InsightDirectedExample.cs
@@ -0,0 +1,39 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+namespace VowganVR
+{
+    [UdonBehaviourSyncMode(BehaviourSyncMode.Manual)]
+    public class InsightDirectedExample : UdonSharpBehaviour
+    {
+        
+        public bool BlendCamera;
+        public bool LockPlayer = true;
+        public float Duration = 3;
+        public InsightController Controller;
+        public Transform Target;
+        public Transform LookAt;
+        
+        private bool directing;
+        
+        
+        public override void Interact()
+        {
+            if (directing) return;
+            directing = true;
+            Controller.StartDirection(Target, LookAt, BlendCamera, LockPlayer);
+            SendCustomEventDelayedSeconds(nameof(EndDirection), Duration);
+        }
+        
+        public void EndDirection()
+        {
+            directing = false;
+            Controller.EndDirection(LockPlayer);
+        }
+        
+        
+    }
+}
