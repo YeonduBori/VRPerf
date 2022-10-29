7949bd8ccd40fab2c230b45fc656ee773cc384df
dongho shin <dongho5309@belivvr.com>
2022-10-29 14:59:11 +0900

    Update Cinemachine Test Scene with 3rd preview

--- Assets/CinemachineTest/TestScene_UdonProgramSources/CameraController.cs
+++ Assets/CinemachineTest/TestScene_UdonProgramSources/CameraController.cs
@@ -5,36 +5,49 @@ using UnityEngine;
 using VRC.SDKBase;
 using VRC.Udon;
 
+[UdonBehaviourSyncMode(BehaviourSyncMode.Manual)]
 public class CameraController : UdonSharpBehaviour
 {
-    public GameObject mainCamera;
-    public GameObject[] managedObjects;
-    public UdonBehaviour testUdonObject;
+    [SerializeField] private Camera mainCamera;
+    [SerializeField] private GameObject[] managedObjects;
+    [SerializeField] private UdonBehaviour testUdonObject;
 
+    private void Start()
+    {
+        mainCamera.enabled = false;
+    }
 
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            Debug.Log("main Camera Toggled!");
+            bool isCamActive = mainCamera.enabled;
+            mainCamera.enabled = !isCamActive;
+        }
 
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            if (managedObjects.Length >= 2)
+            {
+                if (managedObjects[0].activeSelf)
+                {
+                    managedObjects[0].SetActive(false);
+                    managedObjects[1].SetActive(true);
+                }
+                else
+                {
+                    managedObjects[0].SetActive(true);
+                    managedObjects[1].SetActive(false);
+                }
+            }
+
+        }
+    }
     public override void Interact()
     {
         Debug.Log("Switch Camera");
 
-        if (!mainCamera.activeSelf)
-        {
-            mainCamera.SetActive(true);
-        }
-
-        if (managedObjects.Length >= 2)
-        {
-            if (managedObjects[0].activeSelf)
-            {
-                managedObjects[0].SetActive(false);
-                managedObjects[1].SetActive(true);
-            }
-            else
-            {
-                managedObjects[0].SetActive(true);
-                managedObjects[1].SetActive(false);
-            }
-        }
 
         testUdonObject.SendCustomEvent("TestFunction");
     }
